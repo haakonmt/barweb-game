c3e7d4b8fd0088b0b5e741dee8eda7c8aa50fbfa
Håkon Meyer Tørnquist <haakonmt@stud.ntnu.no>
2016-11-04 20:12:33 +0100

    Initial commit

---
+++ desktop/src/no/studentmediene/barwebgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package no.studentmediene.barwebgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import no.studentmediene.barwebgame.BarwebGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BarwebGame(), config);
+	}
+}
